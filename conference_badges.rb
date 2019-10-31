4603cacee81607efdfdab4bb0635414de4dca937
vy vo <tuonggvy.vo@gmail.com>
2019-10-31 03:52:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,9 @@
 # Write your code here.
 def badge_maker(name)
-  puts "Hello, my name is #{name}"
+  puts "Hello, my name is #{name}."
 end
 
+def batch_badge_creator(names)
+  badge_messages[]
+  badge_messages.push()
+end
