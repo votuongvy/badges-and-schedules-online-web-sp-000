2c0ff6a697c36214010e5e340b18984d22a65944
vy vo <tuonggvy.vo@gmail.com>
2019-10-31 04:00:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,9 +5,8 @@ end
 
 
 def batch_badge_creator(names)
-  badge_messages[]
-  names.each do |name|
-  badge_messages.push(badge_maker(name))
+  names.collect do |name|
+  badge_messages.
   end
 end
 
